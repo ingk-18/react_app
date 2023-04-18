261a6e2b86b050af6494edeeba909ab476d7d3be
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-04-19 08:43:31 +0900

    rafce試す

---
+++ src/Hoge.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hoge = () => {
+  return (
+    <div>Hoge</div>
+  )
+}
+
+export default Hoge
+
+
+import React from 'react'
+
+const Hoge = () => {
+  return (
+    <div>Hoge</div>
+  )
+}
+
+export default Hoge
