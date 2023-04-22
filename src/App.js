bcb128224eae970e5a73f83a7d3b0142be9b4d76
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-04-22 11:09:01 +0900

    uuid追加 チェックボックス反転操作 ボタンが押されたらタスクの追加

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import TodoList from "./TodoList";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 
 function App() {
   //todosが追加ボタンした状態を管理するオブジェクト
   //useStateオブジェクトの更新を監視するフックス
-  const [todos] = useState([
+  const [todos, setTodos] = useState([
     { id: 1, name: "タスク１", completed: false}
   ]);
+
+  //useRefを使ってテキストエリアの要素を取得する
+  const todoNameRef = useRef();
+
+  //ボタンが押されたらタスクの追加
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    // console.log(todoNameRef.current.value);
+    setTodos((prevTodos) => {
+    return [...prevTodos, {id: uuidv4(), name: name, completed: false}]
+    });
+    todoNameRef.current.value = null;
+  };
+
+  // チェックボックスを反転させる関数
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
 
   return (
     <>
         {/* TodoListコンポーネントを呼び出してプロップスを渡す */}
-        <TodoList todos={todos}/>
-        <input type="text" name="" id="" />
-        <button>ADD</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+        <input type="text" ref={todoNameRef}/>
+        <button onClick={handleAddTodo}>ADD</button>
         <button>DELETE</button>
         <div>残りのタスク:0</div>
     </>
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const Todo = ({todo}) => {
+const Todo = ({ todo, toggleTodo }) => {
+  // チェックボックスクリックされたら、反転させる関数に値を渡す
+  const handleTodoClick = () => {
+    toggleTodo(todo.id);
+  }
+
   return (
     <div>
       <label>
-        <input type="checkbox"/>
+        <input 
+          type="checkbox"
+          checked={todo.completed}
+          readOnly
+          onChange={handleTodoClick}/>
       </label>
       {todo.name}
     </div>
